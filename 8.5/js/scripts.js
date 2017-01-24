5883d1dce9b91394bb1275e80d3da1bee6877b7d
FryderykG <eszkimos@gmail.com>
2017-01-24 20:55:03 +0100

    add one statement instead of three

--- 8.5/js/scripts.js
+++ 8.5/js/scripts.js
@@ -5,11 +5,4 @@ var a = 6,
     value = (a * a) + (2 * a * b) - (b * b);
 
 window.console.log('Wynik: ' + value);
-
-if (value === 0) {
-    window.console.log('Value równa się ZERO');
-} else {
-    window.console.log('Value jest różna od ZERA');
-}
-
-window.console.log(value > 0 ? "wynik dodatni" : "wynik ujemny");
+window.console.log(value === 0 ? 'zero' : (value > 0) ? 'wynik dodatni' : 'wynik ujemny');
