e748f68d842939212621257672b9b4761c98297c
FryderykG <eszkimos@gmail.com>
2017-01-24 19:27:17 +0100

    add exercise 8.4

--- 8.1/js/scripts.js
+++ 8.1/js/scripts.js
@@ -1,13 +1,24 @@
 /**
  * Created by frycek on 24.01.17.
  */
-var your_name = window.prompt('Wpisz swoje imię');
+var yourName = window.prompt('Wpisz swoje imię');
 
-// window.alert('Witaj, ' + your_name);
-window.console.log('Witaj, ' + your_name);
+// window.alert('Witaj, ' + yourName);
+window.console.log('Witaj, ' + yourName);
+
+// exercise 8.3
 
 /*
  This is a block comment
  */
 
 // and another, this time line comment
+
+// exercise 8.4 Triangle Area
+
+var a = 5,
+    h = 6,
+    triangleArea = a * h / 2;
+
+window.console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' +
+                    h + ' wynosi: ' + triangleArea);
---
+++ 8.4/js/scripts.js
@@ -0,0 +1,11 @@
+// exercise 8.4 Triangle Area
+
+var a = window.prompt("Podaj długość podstawy trójkąta:"),
+    h = 6,
+    triangleArea = a * h / 2;
+
+window.console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' +
+                    h + ' wynosi: ' + triangleArea);
+
+document.write('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' +
+                h + ' wynosi: ' + triangleArea);
