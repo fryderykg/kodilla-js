c6a0af6be7451c1942ddd6f8d37f95f451770c18
FryderykG <eszkimos@gmail.com>
2017-01-24 18:58:33 +0100

    change code due to EsLint add comments to the code

--- 8.1/js/scripts.js
+++ 8.1/js/scripts.js
@@ -1,7 +1,13 @@
 /**
  * Created by frycek on 24.01.17.
  */
-var name = prompt('Wpisz swoje imię');
+var your_name = window.prompt('Wpisz swoje imię');
 
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+// window.alert('Witaj, ' + your_name);
+window.console.log('Witaj, ' + your_name);
+
+/*
+ This is a block comment
+ */
+
+// and another, this time line comment
