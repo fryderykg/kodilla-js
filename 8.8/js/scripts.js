69eaceafae58b26af4c7e526eb4989ea57ee8138
FryderykG <eszkimos@gmail.com>
2017-01-25 10:28:40 +0100

    correct exercise 8.7 Arrays

--- 8.8/js/scripts.js
+++ 8.8/js/scripts.js
@@ -3,17 +3,5 @@
 var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.",
     animal = "Zielone słonie".toUpperCase();
 
-var text = text.replace("Papugi", animal);
-
-
-
-
-
-
-
-
-
-
-
+text = text.replace("Papugi", animal);
 console.log(text.substr(0, text.length/2));
-// console.log(text.indexOf("Papugi"));
