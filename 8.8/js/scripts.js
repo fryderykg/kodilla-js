3f8bd511f530769ce29dcf0853dd3416439deee4
FryderykG <eszkimos@gmail.com>
2017-01-25 10:12:40 +0100

    add exercise 8.8 strings

---
+++ 8.8/js/scripts.js
@@ -0,0 +1,19 @@
+// exercise 8.7  Strings
+
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.",
+    animal = "Zielone słonie".toUpperCase();
+
+var text = text.replace("Papugi", animal);
+
+
+
+
+
+
+
+
+
+
+
+console.log(text.substr(0, text.length/2));
+// console.log(text.indexOf("Papugi"));
